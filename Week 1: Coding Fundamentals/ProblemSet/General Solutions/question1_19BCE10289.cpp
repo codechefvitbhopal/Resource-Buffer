07255db763570a19ddb74f6e3bbdcb552b92ef29
V Surya Kumar <kumarsurya.2001@gmail.com>
2021-02-13 21:00:12 +0530

    Create question1_19BCE10289.cpp

---
+++ Week 1: Coding Fundamentals/ProblemSet/General Solutions/question1_19BCE10289.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+  cout<<"This for testing purpose on how to upload to this repo, Thankyou"<<endl;
+}
+
+int main(){
+  int t;
+  while(t--){
+    solve();
+  }
+  return 0;
+}
